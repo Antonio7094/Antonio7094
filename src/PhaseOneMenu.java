d13bcbdccd989d25dceef9e7b6f00ca6f68ea9e6
Antonio7094 <107634477+Antonio7094@users.noreply.github.com>
2022-07-12 14:10:37 -0500

    Initial Phase1

--- src/PhaseOneMenu.java
+++ src/PhaseOneMenu.java
@@ -1,5 +1,6 @@
 //Antonio A Guzman 1001647094
 import java.util.Scanner;
+import java.util.Random;
 
 
 
@@ -16,13 +17,26 @@ public class PhaseOneMenu {
         int statchoice;
         int choice2=0;
 
+        int D=0;
+        int D2=0;
+
+        String WPCH1 =" ";
+        String DCTP1 =" ";
+        int BNS1 = 0;
+        String WPNM1;
+
+        String WPCH2 =" ";
+        String DCTP2 =" ";
+        int BNS2 = 0;
+        String WPNM2;
+
         if(choice==1)
         {
             System.out.println("Choose from the following options.");
-            //Scanner PlayerSelect = new Scanner(System.in);
+
             System.out.println("One Player: 1");
             System.out.println("Two players: 2");
-            //System.out.println("Back : 3");
+
             String Players = UserSelectionMenu.nextLine();
             choice2 = Integer.parseInt(Players);
         }
@@ -42,20 +56,18 @@ public class PhaseOneMenu {
             int X=0;
             int H=50;
             int S=0;
-            int D=0;
             int C=0;
             int pointstouse=10;
             Player p = new Player(n,L,A,X,H,S,D,C);
             System.out.println("Player 1 enter your name: ");
-            //Scanner PlayerName = new Scanner(System.in);
+
              p.name = UserSelectionMenu.nextLine();
-            //System.out.println("Player 1: "+ p.name);
+
             System.out.printf("Player 1 Name: %s, Level: %d, Armour: %d, Experience: %d, Health: %d, Strength: %d, Dexterity: %d, Constitution: %d",p.name,L,A,X,H,S,D,C);
             System.out.println(" ");
             System.out.println("Use 10 points to improve your attributes.");
             System.out.println("Select from the following options to increase your attributes +1 at a time.");
-            //System.out.println("1. +1 Armor");
-            //System.out.println("2. +1 Health");
+
             System.out.println("1. +1 Strength");
             System.out.println("2. +1 Dexterity");
             System.out.println("3. +1 Constitution");
@@ -76,8 +88,7 @@ public class PhaseOneMenu {
             pointstouse--;
             System.out.printf("Player 1 Name: %s, Strength: %d, Dexterity: %d, Constitution: %d",p.name,S,D,C);
             System.out.println(" ");
-                //System.out.println("1. +1 Armor");
-                //System.out.println("2. +1 Health");
+
                 System.out.println("1. +1 Strength");
                 System.out.println("2. +1 Dexterity");
                 System.out.println("3. +1 Constitution");
@@ -96,21 +107,17 @@ public class PhaseOneMenu {
             int X=0;
             int H=50;
             int S=0;
-            int D=0;
             int C=0;
             int pointstouse=10;
             Player p1 = new Player(n,L,A,X,H,S,D,C);
             System.out.print("Player 1 enter your name: ");
             p1.name = UserSelectionMenu.nextLine();
-            //Scanner PlayerName = new Scanner(System.in);
-            //String Name = UserSelectionMenu.nextLine();
-            //System.out.println("Player 1: "+ p1.name);
+
             System.out.printf("Player 1 Name: %s, Level: %d, Armour: %d, Experience: %d, Health: %d, Strength: %d, Dexterity: %d, Constitution: %d",p1.name,L,A,X,H,S,D,C);
             System.out.println(" ");
             System.out.println("Use 10 points to improve your attributes.");
             System.out.println("Select from the following options to increase your attributes +1 at a time.");
-            //System.out.println("1. +1 Armor");
-            //System.out.println("2. +1 Health");
+
             System.out.println("1. +1 Strength");
             System.out.println("2. +1 Dexterity");
             System.out.println("3. +1 Constitution");
@@ -131,8 +138,7 @@ public class PhaseOneMenu {
                 pointstouse--;
                 System.out.printf("Player 1 Name: %s, Strength: %d, Dexterity: %d, Constitution: %d",p1.name,S,D,C);
                 System.out.println(" ");
-                //System.out.println("1. +1 Armor");
-                //System.out.println("2. +1 Health");
+
                 System.out.println("1. +1 Strength");
                 System.out.println("2. +1 Dexterity");
                 System.out.println("3. +1 Constitution");
@@ -153,25 +159,21 @@ public class PhaseOneMenu {
             int X2=0;
             int H2=50;
             int S2=0;
-            int D2=0;
             int C2=0;
             Player p2 = new Player(n2,L2,A2,X2,H2,S2,D2,C2);
             System.out.print("Player 2 enter your name: ");
             p2.name = UserSelectionMenu.nextLine();
-            //Scanner PlayerName = new Scanner(System.in);
-            //String Name2 = UserSelectionMenu.nextLine();
-            //System.out.println("Player 2: "+ p2.name);
+
             System.out.printf("Player 2 Name: %s, Level: %d, Armour: %d, Experience: %d, Health: %d, Strength: %d, Dexterity: %d, Constitution: %d",p2.name,L2,A2,X2,H2,S2,D2,C2);
             System.out.println(" ");
             System.out.println("Use 10 points to improve your attributes.");
             System.out.println("Select from the following options to increase your attributes +1 at a time.");
-            //System.out.println("1. +1 Armor");
-            //System.out.println("2. +1 Health");
+
             System.out.println("1. +1 Strength");
             System.out.println("2. +1 Dexterity");
             System.out.println("3. +1 Constitution");
             System.out.println("Points remaining : "+ pointstouse);
-            //int statchoice = 0;
+
             while (pointstouse>0)
             {
                 String AttributeChoice = UserSelectionMenu.nextLine();
@@ -188,8 +190,7 @@ public class PhaseOneMenu {
                 pointstouse--;
                 System.out.printf("Player 2 Name: %s, Strength: %d, Dexterity: %d, Constitution: %d",p2.name,S2,D2,C2);
                 System.out.println(" ");
-                //System.out.println("1. +1 Armor");
-                //System.out.println("2. +1 Health");
+
                 System.out.println("1. +1 Strength");
                 System.out.println("2. +1 Dexterity");
                 System.out.println("3. +1 Constitution");
@@ -201,22 +202,10 @@ public class PhaseOneMenu {
             System.out.printf("Player 2 Name: %s, Level: %d, Armour: %d, Experience: %d, Health: %d, Strength: %d, Dexterity: %d, Constitution: %d",p2.name,L2,A2,X2,H2,S2,D2,C2);
             System.out.println(" ");
         }
-//            else if (choice2==3){
-//            System.out.println("Main Menu");
-//            System.out.println(" ");
-//            System.out.println("Choose from the following options.");
-//            Scanner UserSelectionMenu2 = new Scanner(System.in);
-//            System.out.println("Start Game : 1");
-//            System.out.println("Exit : 2");
-//            String Menuchoice2 = UserSelectionMenu2.nextLine();
-//            int choice3 = Integer.parseInt(Menuchoice2);
-        //}
 
-        if (choice==1){
-            String WPCH1 =" ";
-            String DCTP1 =" ";
-            int BNS1 = 0;
-            String WPNM1;
+
+        if (choice2==1){
+
             System.out.println(" ");
             System.out.println("Player 1 Select your Weapon:");
             System.out.println(" ");
@@ -262,7 +251,8 @@ public class PhaseOneMenu {
 
 
         }
-        else if (choice==2){
+        else if (choice2==2){
+
             System.out.println(" ");
             System.out.println("Player 1 Select your Weapon:");
             System.out.println(" ");
@@ -271,6 +261,41 @@ public class PhaseOneMenu {
             System.out.println("3. Warhammer");
             System.out.println("4. Shortsword");
             System.out.println("5. Dagger");
+            Weapons W1 = new Weapons(WPCH1,DCTP1,BNS1);
+            WPNM1 = UserSelectionMenu.nextLine();
+            switch(WPNM1){
+                case "1":
+                    WPCH1 = "Greataxe";
+                    DCTP1 = "1d12";
+                    BNS1 = 0;
+                    break;
+                case "2":
+                    WPCH1 = "Longsword";
+                    DCTP1 = "1d10";
+                    BNS1 = 1;
+                    break;
+                case "3":
+                    WPCH1 = "Warhammer";
+                    DCTP1 = "1d8";
+                    BNS1 = 2;
+                    break;
+                case "4":
+                    WPCH1 = "Shortsword";
+                    DCTP1 = "1d6";
+                    BNS1 = 3;
+                    break;
+                case "5":
+                    WPCH1 = "Dagger";
+                    DCTP1 = "1d4";
+                    BNS1 = 4;
+                    break;
+            }
+            W1.name = WPCH1;
+            W1.Dicetype = DCTP1;
+            W1.Bonus = BNS1;
+            System.out.printf("PLayer 1 chosen weapon: %s",WPCH1);
+            System.out.println(" ");
+
 
             System.out.println(" ");
             System.out.println("Player 2 Select your Weapon:");
@@ -280,11 +305,72 @@ public class PhaseOneMenu {
             System.out.println("3. Warhammer");
             System.out.println("4. Shortsword");
             System.out.println("5. Dagger");
-
-
+            Weapons W2 = new Weapons(WPCH2,DCTP2,BNS2);
+            WPNM2 = UserSelectionMenu.nextLine();
+            switch(WPNM2){
+                case "1":
+                    WPCH2 = "Greataxe";
+                    DCTP2 = "1d12";
+                    BNS2 = 0;
+                    break;
+                case "2":
+                    WPCH2 = "Longsword";
+                    DCTP2 = "1d10";
+                    BNS2 = 1;
+                    break;
+                case "3":
+                    WPCH2 = "Warhammer";
+                    DCTP2 = "1d8";
+                    BNS2 = 2;
+                    break;
+                case "4":
+                    WPCH2 = "Shortsword";
+                    DCTP2 = "1d6";
+                    BNS2 = 3;
+                    break;
+                case "5":
+                    WPCH2 = "Dagger";
+                    DCTP2 = "1d4";
+                    BNS2 = 4;
+                    break;
+            }
+            W2.name = WPCH2;
+            W2.Dicetype = DCTP2;
+            W2.Bonus = BNS2;
+            System.out.printf("PLayer 2 chosen weapon: %s",WPCH2);
+            System.out.println(" ");
+            System.out.println(" ");
         }
+        System.out.println("Players will now roll a 20d die to determine who will go first, player one you roll first.");
+        System.out.println(" ");
+        System.out.println("Rolling.......");
+        int RollResult;
+        Random r = new Random();
+        RollResult = r.nextInt(20+1);
+        System.out.println("PLayer 1 rolled :"+RollResult);
+        RollResult += D;
+        System.out.println("We will now add your total dexterity to the roll for a total of : "+RollResult);
+        System.out.println(" ");
+        System.out.println("Player 2 now rolling.....");
+        int RollResult2;
+        Random r2 = new Random();
+        RollResult2 = r2.nextInt(20+1);
+        System.out.println("PLayer 2 rolled :"+RollResult2);
+        RollResult2 += D2;
+        System.out.println("We will now add your total dexterity to the roll for a total of : "+RollResult2);
 
-
+        if (RollResult > RollResult2){
+            System.out.println(" ");
+            System.out.println("Player 1 will go first.");
+        }
+        else if (RollResult < RollResult2){
+            System.out.println(" ");
+            System.out.println("Player 2 will go first.");
+        }
+        else if (RollResult == RollResult2){
+            System.out.println(" ");
+            System.out.println("Both players rolled the same, thus need to re-roll.");
+        }
 
     }
 }
